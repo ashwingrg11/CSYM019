72aeb61d696b6996a7756fb21b56832a47b3d515
Ashwin Gurung <ashwin.gurung@cloudyfox.com>
2022-03-03 17:02:21 +0000

    click event added on button

--- Week4/js/app.js
+++ Week4/js/app.js
@@ -6,3 +6,21 @@ function print4() {
 }
 print4();
 print4();
+
+
+// JavaScript Exercise
+
+// update the content of heading
+function updateContents() {
+  var headingElm = document.getElementById('pageHeading');
+  headingElm.firstChild.nodeValue = 'New heading contents from JavaScript';
+  var paragraphElm = document.getElementById('pageParagraph');
+  paragraphElm.firstChild.nodeValue = 'New paragraph contents from JavaScript';
+}
+
+function myLoadFunction() {
+  var elm = document.getElementById('updateBtn');
+  elm.addEventListener('click', updateContents);
+}
+document.addEventListener('DOMContentLoaded', myLoadFunction);
+//document.addEventListener('click', updateContents);
