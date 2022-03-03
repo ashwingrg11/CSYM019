b7b2b3ee91caad471d0b8fe9f02cf02705614885
Ashwin Gurung <ashwin.gurung@cloudyfox.com>
2022-03-03 16:02:40 +0000

    console.log is used instead of alert

--- Week4/js/app.js
+++ Week4/js/app.js
@@ -1,8 +1,8 @@
 function print4() {
-  for (var i = 0; i < 5; i++) {
+  for (var i = 1; i <= 5; i++) {
     // alert(i+1);
-    console.log(i+1);
+    console.log(i);
   }
 }
 print4();
-// print4();
+print4();
